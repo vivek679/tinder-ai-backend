abb3b46e6d0822089cda62f2ac6e15f01118d90d
learner <learning@example.com>
2024-10-17 01:52:45 +0530

    Match logic

---
+++ src/main/java/com/learning/tinderaibackend/notification/NotificationService.java
@@ -0,0 +1,15 @@
+package com.learning.tinderaibackend.notification;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class NotificationService {
+    public void sendMatchNotification(String profileId1, String profileId2) {
+
+    }
+}
